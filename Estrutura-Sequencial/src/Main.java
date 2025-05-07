c9ce641d212700937d2fce4cd89b64d6533d5b2b
ThiagoVini-S <th.vinialmeida@gmail.com>
2025-05-07 20:40:28 -0300

    Feat: Exercicio de estrutura sequencial

---
+++ Estrutura-Sequencial/src/Main.java
@@ -0,0 +1,17 @@
+/*
+    Fazer um programa para ler as medidas da largura e comprimento de um terreno
+    retangular com uma casa decimal, bem como o valor do metro quadrado do terreno
+    com duas casas decimais. Em seguida, o programa deve mostrar o valor da área do
+    terreno, bem como o valor do preço do terreno, ambos com duas casas decimais,
+    conforme exemplo.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
